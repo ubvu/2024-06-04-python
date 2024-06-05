b36dc036b2dc4662f1bca03fbb58ff94825b571c
tijs bliek <bliek@uva.nl>
2024-06-05 14:25:12 +0200

    gitautosync update 113

--- notebooks/reverse.py
+++ notebooks/reverse.py
@@ -1,5 +1,5 @@
 def reverse_and_complement( dna_strand ):
-    Comp = {“a”:”t”, “c”:”g”, “g”:”c”,”t”:”a”}
+    Comp = {'a':'t', 'c':'g', 'g':'c','t':'a'}
     comp_strand = “”
     for base in dna_strand:
         comp_strand = comp_strand + Comp[base.lower()]
