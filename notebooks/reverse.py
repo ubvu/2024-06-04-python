2179acec7402f3b40c10f371e75114ed0c778866
tijs bliek <bliek@uva.nl>
2024-06-06 09:57:13 +0200

    Update reverse.py
    removed text

--- notebooks/reverse.py
+++ notebooks/reverse.py
@@ -1,7 +1,1 @@
-def reverse_and_complement( dna_strand ):
-    Comp = {'a':'t', 'c':'g', 'g':'c','t':'a'}
-    comp_strand = ''
-    for base in dna_strand:
-        comp_strand = comp_strand + Comp[base.lower()]
-    return comp_strand 
-        
+
